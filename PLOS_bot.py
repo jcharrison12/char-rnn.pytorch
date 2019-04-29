e717187a5df10acfc6ad8da33c4540ffa496bb60
Jordan <jcharrison12@gmail.com>
2019-04-28 19:14:26 -0500

    Update PLOS_bot.py

--- PLOS_bot.py
+++ PLOS_bot.py
@@ -8,10 +8,10 @@ Created on Fri Apr 26 14:44:57 2019
 import tweepy
 import run_generate
 
-consumer_key = 'RgrhdfWPd2fzLc0nEjoYLMAVK'
-consumer_secret = 'OtFgSDNipdVtAviQUx21z31YBQQ5aGsx7zYRDua8ilNQXp5PDp'
-access_token = '1121853140159148033-klLhu8C2SduEtVyITow2YRCAeMegbd'
-access_token_secret = 'L9jCFzToQaisqPlVPRj74acimTamDFhFiv7OHFgoxyO2t'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
