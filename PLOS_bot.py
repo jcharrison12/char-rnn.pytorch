71e665c4cf55e46033396d891518981d2721968b
Jordan <jcharrison12@gmail.com>
2019-04-28 19:46:18 -0500

    Update PLOS_bot.py

--- PLOS_bot.py
+++ PLOS_bot.py
@@ -7,19 +7,26 @@ Created on Fri Apr 26 14:44:57 2019
 
 import tweepy
 import run_generate
+import sys
 
-consumer_key = ''
-consumer_secret = ''
-access_token = ''
-access_token_secret = ''
+def main():
+  consumer_key = sys.argv[1]
+  consumer_secret = sys.argv[2]
+  access_token = sys.argv[3]
+  access_token_secret = sys.argv[4]
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
+  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+  auth.set_access_token(access_token, access_token_secret)
+  api = tweepy.API(auth)
 
-user = api.me()
-print(user.name)
-
-tweet=run_generate.clean_title("all_titles.pt", "all_titles.txt.")
-api.update_status(tweet)
-print('Done')
+  user = api.me()
+  print(user.name)
+  
+  pt_file= sys.argv[5]
+  text_file= sys.argv[6]
+  tweet=run_generate.clean_title(pt_file, text_file)
+  api.update_status(tweet)
+  print('Done')
+  
+if __name__ == '__main__':
+	main()
