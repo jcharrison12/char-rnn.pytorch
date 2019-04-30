17c1c60be2f7a0f5b0cfc8ae7a95da60da2afc1f
Jordan <jcharrison12@gmail.com>
2019-04-29 19:14:37 -0500

    Update run_generate.py

--- run_generate.py
+++ run_generate.py
@@ -15,21 +15,21 @@ import model
 import generate
 
 def clean_title(ptfile, textfile):
-    nltk.download('stopwords')
+    nltk.download('stopwords')   #get stopwords (in, a, the, is, etc.)
     from nltk.corpus import stopwords
     en_stops = set(stopwords.words('english'))
 
-    random_line=random.choice(list(open(textfile, encoding="utf8")))
-    first_word=random_line.split(' ', 1)[0]
+    random_line=random.choice(list(open(textfile, encoding="utf8")))  
+    first_word=random_line.split(' ', 1)[0]  #choose a random first word of a title in the given text file
     
-    pt=torch.load(ptfile)
-    x=generate.generate(pt, first_word)
-    x=x.replace('\n', ' ')
-    title_list=x.split(' ', -1)
+    pt=torch.load(ptfile) #load/process pt model file
+    x=generate.generate(pt, first_word) #priming string is randomized first word in text
+    x=x.replace('\n', ' ') #remove new lines
+    title_list=x.split(' ', -1) #last word of title is frequently cut off, so always remove last word
     title_list.pop()
     b2string=" "
     y=b2string.join(title_list)
-    if title_list[-1] in en_stops:
+    if title_list[-1] in en_stops: #if second to last word is a stopword, remove that one too
         title_list.pop()
         y=b2string.join(title_list)
     return(y)
